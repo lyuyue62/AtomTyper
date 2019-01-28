665d1cccb972373edc8ca4b667268f596a500040
Seonghoon Kim <deeperbass1@gmail.com>
2019-01-28 16:39:43 -0500

    Add files via upload

---
+++ LinkedAtomGroup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+class LinkedAtomGroup(object):
+    i = int()
+    j = int()
+    linked_atoms = str()
+    linked_bonds = str()
+
+    def __init__(self):
+        self.i = self.j = -1
+
