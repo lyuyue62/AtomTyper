3be376c1e9c0a033a1c324360c7d3673f5091b5c
Seonghoon Kim <deeperbass1@gmail.com>
2019-01-28 16:35:29 -0500

    Add files via upload

---
+++ Bond.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+class Bond(object):
+    i = int()
+    j = int()
+    order = int()
+    bondType = str()
+
+    #  *** For atomic partial charge assignment ***
+    increment = float()
+    index = int()
+
+    def __init__(self):
+        self.i = -1
+        self.j = -1
+        self.order = 0
+        self.bondType = None
+        self.increment = 0
+        self.index = -1
+
